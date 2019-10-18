d30d291f28159246485f568dd7babdff14e936ca
owenabrams <owenabrams@gmail.com>
2019-10-18 02:34:28 -0700

    Hello
    Hello

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  GOOGLE_API_KEY: 'AIzaSyAE_8C8cT6kyLUKNdG0-9xtI5WEmvQfgLU'
+};
+
+
+//     AIzaSyCzvPIX8w3OYxUC1oyqFn_2AjGu3dYLzMk
+
+//  AIzaSyAE_8C8cT6kyLUKNdG0-9xtI5WEmvQfgLU
